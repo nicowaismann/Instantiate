73986e1a50dd3b5d9d969b7476999654d7bc8895
nicowaismann <46961446@est.ort.edu.ar>
2022-05-04 15:50:32 -0300

    Initial commit

---
+++ Assets/Clonar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clonar : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void cloneObject()
+    {
+
+
+        while (i != 3)
+        {
+            i += 1;
+            Instantiate(ObjectToClone);
+        }
+        i = 0;
+    }
+}
